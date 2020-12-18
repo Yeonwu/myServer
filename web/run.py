2febfa131929614e6cc7bf255f0a89a705cd0fa5
Yeonwu Oh <yeonwu319@gmail.com>
2020-12-18 12:14:14 +0000

    push test 2

--- web/run.py
+++ web/run.py
@@ -2,13 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello~! I love python!'
+    return 'Hello World'
 
-@app.route('/cat')
-def cakes():
-    return 'This is cat'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
